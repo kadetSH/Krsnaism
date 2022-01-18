c2bb021ab6ef32c51bbd22046f826ac91a391b2c
Kadet <0kadet0@gmail.comgtht>
2022-01-18 22:58:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/krsnaism/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.krsnaism
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/krsnaism/StartActivity.kt
@@ -0,0 +1,11 @@
+package com.example.krsnaism
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
